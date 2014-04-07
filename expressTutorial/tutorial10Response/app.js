7bc8982afcfe25eef770c79b895901c0e8f25eb5
jorgep19 <jorgelpaez19@gmail.com>
2014-04-07 01:45:23 -0400

    just missing the mongo tutorial

--- expressTutorial/tutorial10Response/app.js
+++ expressTutorial/tutorial10Response/app.js
@@ -19,10 +19,36 @@ app.get('/', function(req, res){
     // res.send(404, 'not found');
 
     // you can use this to sen a json directly
-    // var myObject = { stuff: 'value'};
-    // res.json(myObject);
+    var myObject = { stuff: 'value'};
+    res.json(myObject);
 
-    res.type('image/png)')
+    // specify the type of the response
+    // res.type('image/png)').send('This is picture');
+
+    // setup frst then get the appropiate format for the accepted response
+//    res.format({
+//        html: function() { res. send("<h> Body </h>"); },       // usually default
+//        json: function() { res.json({ message: 'body' } ) },    // curl localhost:3000 -H "accept: application/json"
+//        text: function() { res.send('body') }                   // curl localhost:3000 -H "accept: text/plain"
+//    });
+});
+
+//redirecting calls
+app.get('/home', function(req, res){
+    // also you can setup the status of the response here
+    // 302 means redireccted temperoraly
+    res.status(302).redirect('/');
+
+    // to check you can set the headers using
+    // res.set / res,header
+
+    // Addition methos to checkout
+    // send file
+    // download
+    // attachment
+    // links
+    // clearCookie
+    // cookies
 });
 
 http.createServer(app).listen(app.get('port'), function(){
